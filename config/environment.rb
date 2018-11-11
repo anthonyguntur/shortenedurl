628cab9c992caa8859727418e77b5d4ecb3fd1be
Anthony Guntur <admin-technology@figsinc.com>
2018-11-12 01:55:15 +0800

    Sqlite3 issue

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,14 @@ require 'bundler/setup'
 require 'rack-flash'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :adapter => "postgresql",
+  :database => "shorten_db_development"
 )
 
 require_all 'app'
